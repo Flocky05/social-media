724800764cea94f8bf77d7d8ed06218b24217a26
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-01-24 22:41:57 +0600

    update Header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,64 @@
+import React from "react";
+import img1 from "../../imges/figma/header/Rectangle 2.10 (1).png";
+import img2 from "../../imges/figma/header/Rectangle 2.10.png";
+import img3 from "../../imges/figma/header/Rectangle 2.11.png";
+
+const Header = () => {
+  return (
+    <center className="mt-8">
+      <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
+        <div className="carousel-item">
+          <img
+            alt=" "
+            src="https://placeimg.com/250/180/arch"
+            className="rounded-box"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            alt=" "
+            src="https://placeimg.com/250/180/arch"
+            className="rounded-box"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            alt=" "
+            src="https://placeimg.com/250/180/arch"
+            className="rounded-box"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            alt=" "
+            src="https://placeimg.com/250/180/arch"
+            className="rounded-box"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            alt=" "
+            src="https://placeimg.com/250/180/arch"
+            className="rounded-box"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            alt=" "
+            src="https://placeimg.com/250/180/arch"
+            className="rounded-box"
+          />
+        </div>
+        <div className="carousel-item">
+          <img
+            alt=" "
+            src="https://placeimg.com/250/180/arch"
+            className="rounded-box"
+          />
+        </div>
+      </div>
+    </center>
+  );
+};
+
+export default Header;
--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,3 +1,4 @@
+import Header from "../Components/Header/Header";
 import Main from "../Components/Main/Main";
 
 const { createBrowserRouter } = require("react-router-dom");
@@ -6,5 +7,11 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    children: [
+      {
+        path: "/Home",
+        element: <Header></Header>,
+      },
+    ],
   },
 ]);
