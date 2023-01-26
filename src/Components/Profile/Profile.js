678daf9e087e010255f17c8cdbdedc7694b839f2
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-01-26 21:30:10 +0600

    added Uploade section

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,24 +1,35 @@
 import React from "react";
 import img from "../../imges/profile/profile.jpeg";
 import img2 from "../../imges/profile/bg.jpg";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   return (
-    <section
-      className="bg-no-repeat bg-cover relative bg-center "
-      style={{
-        backgroundImage: `url(${img2})`,
-        height: `720px`,
-      }}
-    >
-      <div className="absolute bottom-5 left-5">
-        <img
-          src={img}
-          alt=""
-          className="w-80 h-80 rounded-full border-4 border-slate-300-500 "
-        />
+    <div>
+      <section
+        className="bg-no-repeat bg-cover relative bg-center "
+        style={{
+          backgroundImage: `url(${img2})`,
+          height: `720px`,
+        }}
+      >
+        <div className="absolute bottom-5 left-5">
+          <img
+            src={img}
+            alt=""
+            className="w-80 h-80 rounded-full border-4 border-slate-300-500 "
+          />
+        </div>
+      </section>
+      <div className="my-4">
+        <Link
+          to="/Uploade"
+          className="bg-green-700 hover:bg-green-900 px-2 py-1 rounded-lg text-white"
+        >
+          Uploaded picture
+        </Link>
       </div>
-    </section>
+    </div>
   );
 };
 
--- src/Router/Router.js
+++ src/Router/Router.js
@@ -3,6 +3,7 @@ import LogOut from "../Components/LogOut/LogOut";
 import Main from "../Components/Main/Main";
 import NewsFeed from "../Components/NewsFeed/NewsFeed";
 import Profile from "../Components/Profile/Profile";
+import Uploade from "../Components/Profile/Upload/Uploade";
 import Login from "../Components/Shared/Login/Login";
 
 const { createBrowserRouter } = require("react-router-dom");
@@ -31,6 +32,10 @@ export const router = createBrowserRouter([
       {
         path: "/Login",
         element: <Login></Login>,
+      },
+      {
+        path: "/Uploade",
+        element: <Uploade></Uploade>,
       },
     ],
   },
