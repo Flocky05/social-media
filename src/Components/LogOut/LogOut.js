4c025cde0d4fd2b02859c169a9bd8e5316124fd5
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-01-26 16:58:29 +0600

    update logOut part

--- src/Components/LogOut/LogOut.js
+++ src/Components/LogOut/LogOut.js
@@ -1,12 +1,15 @@
 import React from "react";
-import video from "../../imges/video/Welcome.mp4";
+import img from "../../imges/LogOut/LogOut.webp";
 
 const LogOut = () => {
   return (
-    <div>
-      <video autoPlay muted loop className="w-full">
-        <source src={video} />
-      </video>
+    <div className="relative">
+      <center className="my-4">
+        <img src={img} className="w-3/4" alt="" />
+      </center>
+      <button className="text-white bg-green-700 rounded-md px-3 py-1 absolute bottom-5 right-10">
+        LogOut
+      </button>
     </div>
   );
 };
