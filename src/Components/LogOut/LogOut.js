74037fd29205f0ea18176233e5eb2a48fd063494
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-01-26 19:19:12 +0600

    Update logOut part

--- src/Components/LogOut/LogOut.js
+++ src/Components/LogOut/LogOut.js
@@ -1,16 +1,28 @@
 import React from "react";
 import img from "../../imges/LogOut/LogOut.webp";
+import img1 from "../../imges/LogOut/background.jpg";
+import { Link } from "react-router-dom";
 
 const LogOut = () => {
   return (
-    <div className="relative">
-      <center className="my-4">
-        <img src={img} className="w-3/4" alt="" />
-      </center>
-      <button className="text-white bg-green-700 rounded-md px-3 py-1 absolute bottom-5 right-10">
-        LogOut
-      </button>
-    </div>
+    <section
+      className="bg-no-repeat bg-cover"
+      style={{
+        backgroundImage: `url(${img1})`,
+      }}
+    >
+      <div className="relative">
+        <center className="my-4">
+          <img src={img} className="w-1/2 py-10" alt="" />
+        </center>
+        <Link
+          to="/Login"
+          className="text-white bg-green-700 rounded-md px-3 py-1 absolute bottom-5 right-10"
+        >
+          LogOut
+        </Link>
+      </div>
+    </section>
   );
 };
 
