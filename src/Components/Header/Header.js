2c294f5627c665142569e98c031c2bb44946ab15
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-08-30 22:29:27 +0600

    added header section

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,28 +1,25 @@
 import React from "react";
-import video from "../../imges/video/Welcome.mp4";
-
 const LogOut = () => {
   return (
     <div>
-      <center>
-        <video autoPlay muted loop className="w-full object-cover">
-          <source src={video} />
-        </video>
-      </center>
       <div>
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero min-h-screen bg-blue-200">
           <div className="hero-content flex-col lg:flex-row-reverse">
             <img
               src="https://img.freepik.com/free-vector/horror-cat-halloween-background_1142-1169.jpg?w=740&t=st=1675708393~exp=1675708993~hmac=badbe7fb32bbb61f6a2c5362dca9853bc20a0f55d491542ba164d5a13d79758b"
               className="max-w-sm rounded-lg shadow-2xl"
               alt=""
             />
-            <div>
-              <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <div className="text-black">
+              <h1 className="text-5xl font-bold ">Enjoy your life</h1>
               <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio
+                deleniti, commodi reiciendis harum consequuntur alias corporis
+                sunt ex nam. A laboriosam ducimus dolor esse quidem
+                exercitationem, aspernatur mollitia voluptatem distinctio, amet
+                sit explicabo corporis laudantium, atque dolorum velit
+                doloremque consequatur earum nulla! Dicta, totam itaque et iure
+                aliquam placeat deleniti!
               </p>
               <button className="btn btn-primary">Get Started</button>
             </div>
